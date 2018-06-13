386176bb14869aac2ff2b81edea847eaa7a4d019
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-13 12:08:50 +0200

    Add click event handler on trigger button in js/script.js

--- js/script.js
+++ js/script.js
@@ -14,4 +14,20 @@ function createTweet(input) {
   if (!quoteAuthor.length) {
     quoteAuthor = "Unknown author";
   }
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+  if (tweetText.length > 140) {
+    getQuote();
+  } else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(quoteText);
+    $('.author').text("Author: " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
+  }
 }
+
+$(document).ready(function() {
+  getQuote();
+  $('.trigger').click(function() {
+    getQuote();
+  })
+});
