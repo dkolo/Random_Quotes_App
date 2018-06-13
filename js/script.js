bc704a0b82b92f2fc6ca642d77b4f5db02ee5630
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-13 12:06:39 +0200

    Add createTweet function implementation in script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&fi
 function getQuote() {
   $.getJSON(quoteUrl, createTweet);
 }
+
+function createTweet(input) {
+  var data = input[0];
+
+  var quoteText = $(data.content).text().trim();
+  var quoteAuthor = data.title;
+
+  if (!quoteAuthor.length) {
+    quoteAuthor = "Unknown author";
+  }
+}
